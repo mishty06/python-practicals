f726f01a36a54a49a53eb1c3f86936d0108be63a
mishty06 <138766992+mishty06@users.noreply.github.com>
2023-09-17 17:42:22 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,14 @@
+
+grade=int(input("Enter your grade : "))
+if grade>=90:
+    print("Your Grade is A !!")
+elif grade>=80 and grade<=89:
+     print("Your Grade is B !!")
+elif grade>=70 and grade<=79:
+     print("Your Grade is C !!")
+elif grade>=60 and grade<=69:
+     print("Your Grade is D !!")
+elif grade>=50 and grade<=59:
+     print("Your Grade is E !!")
+else :
+     print("Your Grade is F !!")
